aa693ca38dfb0e33369bcc5613c9b59ea9324002
Roman Kirillov <roman.kirillov.06@mail.ru>
2026-08-01 16:51:46 +0300

    add#1

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -1,32 +1,71 @@
-import { PostsService } from './posts.service';
+import { PostsService } from "./posts.service";
 
-describe('PostsService', () => {
+describe("PostsService", () => {
   let postsService: PostsService;
 
   beforeEach(() => {
     postsService = new PostsService();
   });
 
-  describe('.findMany', () => {
+  describe(".findMany", () => {
     const posts = [
-      {text: 'Post 1'},
-      {text: 'Post 2'},
-      {text: 'Post 3'},
-      {text: 'Post 4'},
+      { text: "Post 1" },
+      { text: "Post 2" },
+      { text: "Post 3" },
+      { text: "Post 4" },
     ];
 
     beforeEach(() => {
       posts.forEach((post) => postsService.create(post));
     });
 
-    it('should return all posts if called without options', () => {
-      // реализуйте тест-кейс
+    it("should return all posts if called without options", () => {
+      expect(postsService.findMany()).toHaveLength(4);
+      expect(postsService.findMany()).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({ text: "Post 1" }),
+          expect.objectContaining({ text: "Post 2" }),
+          expect.objectContaining({ text: "Post 3" }),
+          expect.objectContaining({ text: "Post 4" }),
+        ]),
+      );
     });
 
-    it('should return correct posts for skip and limit options', () => {
-      // реализуйте тест-кейс
+    it("should return correct posts for skip and limit options", () => {
+      const result = postsService.findMany({
+        skip: 1,
+        limit: 2,
+      });
+
+      expect(result).toHaveLength(2);
+      expect(result).toEqual([
+        expect.objectContaining({ text: "Post 2" }),
+        expect.objectContaining({ text: "Post 3" }),
+      ]);
     });
 
-    // реализуйте недостающие тест-кейсы
+    it("should return correct posts with limit option", () => {
+      const result = postsService.findMany({
+        limit: 2,
+      });
+
+      expect(result).toHaveLength(2);
+      expect(result).toEqual([
+        expect.objectContaining({ text: "Post 1" }),
+        expect.objectContaining({ text: "Post 2" }),
+      ]);
+    });
+
+    it("should return correct posts with skip option", () => {
+      const result = postsService.findMany({
+        skip: 2,
+      });
+
+      expect(result).toHaveLength(2);
+      expect(result).toEqual([
+        expect.objectContaining({ text: "Post 3" }),
+        expect.objectContaining({ text: "Post 4" }),
+      ]);
+    });
   });
 });
